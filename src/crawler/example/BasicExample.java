6771cc53031b249afb385a12544ec798459a027d
Jack Liang <ptdg83@gmail.com>
2017-04-08 10:34:43 +0800

    1st git commit

--- src/crawler/example/BasicExample.java
+++ src/crawler/example/BasicExample.java
@@ -10,7 +10,7 @@ import org.apache.commons.logging.impl.SimpleLog;
  *
  */
 public class BasicExample {
-	// commit test
+	// commit test  1st git commit
 	public static void main(String[] args) {
 
 		// set to debug level
