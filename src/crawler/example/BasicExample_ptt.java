482c52faec2464b0f8af0522853356948938015e
Jack Liang <ptdg83@gmail.com>
2017-04-09 11:50:00 +0800

    ptt content

--- src/crawler/example/BasicExample_ptt.java
+++ src/crawler/example/BasicExample_ptt.java
@@ -2,6 +2,8 @@ package crawler.example;
 
 import com.github.abola.crawler.CrawlerPack;
 import org.apache.commons.logging.impl.SimpleLog;
+import org.jsoup.select.Elements;
+
 
 /**
  * 爬蟲包程式的全貌，就只有這固定的模式
@@ -21,7 +23,7 @@ public class BasicExample_ptt {
 
 		// 遠端資料路徑
 		String uri = "https://www.ptt.cc/bbs/Gossiping/M.1491703320.A.848.html";
-
+/*
 		System.out.println(
 				CrawlerPack.start()
 				
@@ -37,7 +39,18 @@ public class BasicExample_ptt {
 			    // 這兒開始是 Jsoup Document 物件操作
 			    //.select("div.push > span.f1.hl.push-tag, div.push > span.hl.push-tag, div.push > span.f3.push-content")
                 //.select("div.article-metaline > span.article-meta-value")
-				.select("div.push:contains(噓)")
+				.select("div.push:contains(推)")
+
 		);
+	*/
+		Elements jsoup = CrawlerPack.start()
+				.addCookie("over18","1")
+				.getFromHtml(uri)
+				.select("#main-content.bbs-screen.bbs-content");
+		jsoup.select("div,span").remove();
+        System.out.println(
+        		jsoup.text()
+		);
+
 	}
 }
