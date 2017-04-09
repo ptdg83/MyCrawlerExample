0a9fba24b5e7d87d2aa7942e8c470435a189c5b0
Jack Liang <ptdg83@gmail.com>
2017-04-09 17:10:24 +0800

    weather

---
+++ src/crawler/example/CSSSelector_weather.java
@@ -0,0 +1,52 @@
+package crawler.example;
+
+import com.github.abola.crawler.CrawlerPack;
+import org.apache.commons.logging.impl.SimpleLog;
+
+/**
+ * 範例: 使用爬蟲包取得八卦版最後50篇文章的網址
+ *
+ *  Google Chrome 的開發人員工具
+ *  在指定的區塊按右鍵 > Copy > Copy Selector
+ *
+ * @author Abola Lee
+ */
+class CSSSelector_weather {
+
+    public static void main(String[] args) {
+
+        // set to debug level
+        //CrawlerPack.setLoggerLevel(SimpleLog.LOG_LEVEL_DEBUG);
+
+        // turn off logging
+        CrawlerPack.setLoggerLevel(SimpleLog.LOG_LEVEL_OFF);
+
+        // 遠端資料路徑
+        //String uri = "https://tw.yahoo.com";
+        //String uri = "http://new.cpc.com.tw/division/mb/oil-more4.aspx";
+        String uri = "http://e-service.cwb.gov.tw/HistoryDataQuery/DayDataController.do?command=viewMain&station=466910&stname=%25E9%259E%258D%25E9%2583%25A8&datepicker=2017-04-01";
+        System.out.println(
+                CrawlerPack.start()
+
+                        // 參數設定
+                        //.addCookie("key","value")	// 設定cookie
+                        //.setRemoteEncoding("big5")// 設定遠端資料文件編碼
+
+                        // 選擇資料格式 (三選一)
+                        //.getFromJson(uri).toString()
+                        .getFromHtml(uri)
+                        //.getFromXml(uri)
+
+                        // 這兒開始是 Jsoup Document 物件操作
+                        //.select(" li:nth-child(2) > div > span > a > span")
+                        //Weather
+                        .select("#hea_t > table > tbody > tr > td:nth-child(1)")
+                        //.select("#hea_t > table > tbody > tr > td:nth-child(3)")
+                        //.select( cssQuery: "#MyTable > tbody > tr:nth-child(3) > td:nth-child(2)")
+                        //.select("#MyTable > tbody > tr:nth-child(3) > td:nth-child(3)")
+                        //.select("#MyTable > tbody > tr:nth-child(3) > td:nth-child(4)")
+                        //.select("#MyTable > tbody > tr:nth-child(3) > td:nth-child(5)")
+
+        );
+    }
+}
